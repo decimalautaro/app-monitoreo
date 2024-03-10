f02c89fef5057831f60a90a44985b76e22ecef14
Lautaro Decima <decimalautaro@gmail.com>
2024-03-10 03:41:26 -0300

    feat: adding dependency injection

--- src/domain/use-cases/checks/check-services.ts
+++ src/domain/use-cases/checks/check-services.ts
@@ -1,18 +1,24 @@
 interface CheckServiceUseCase {
   execute(url: string): Promise<boolean>;
 }
+type SuccesCallback = () => void;
+type ErrorCallback = (error: string) => void;
 
 export class CheckService {
+  constructor(
+    private readonly succesCallback: SuccesCallback,
+    private readonly errorCallback: ErrorCallback
+  ) {}
   async execute(url: string): Promise<boolean> {
     try {
       const req = await fetch(url);
       if (!req.ok) {
         throw new Error(`Error on check service ${url}`);
       }
-      console.log(`${url} is OK`);
+      this.succesCallback();
       return true;
     } catch (error) {
-      console.log(`${error}`);
+      this.errorCallback(`${error}`);
       return false;
     }
     return true;
--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,13 +1,18 @@
 import { CronJob } from "cron";
 import { CronService } from "./cron/cron-service";
 import { CheckService } from "../domain/use-cases/checks/check-services";
+import { error } from "console";
 export class Server {
   public static start() {
     console.log("Server started...");
 
     CronService.createJob("*/5 * * * * *", () => {
-      // new CheckService().execute("https://google.com");
-      new CheckService().execute("http://localhost:3000");
+      const url = "https://google.com";
+      new CheckService(
+        () => console.log(`${url} is ok`),
+        (error) => console.log(error)
+      ).execute(url);
+      // new CheckService().execute("http://localhost:3000");
     });
   }
 }

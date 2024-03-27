70646bb32ec30d12ccea79cf82b565117bb70bf3
Lautaro Decima <decimalautaro@gmail.com>
2024-03-26 22:59:00 -0300

    feat:send attachments in email

--- src/presentation/email/email-service.ts
+++ src/presentation/email/email-service.ts
@@ -2,10 +2,15 @@ import nodemailer from "nodemailer";
 import { enviroment } from "../../config/enviroment";
 
 interface SendMailOptions {
-  to: string;
+  to: string | string[];
   subject: string;
   htmlBody: string;
-  // attachements:
+  attachements?: Attachement[];
+}
+
+interface Attachement {
+  filename: string;
+  path: string;
 }
 
 export class EmailService {
@@ -19,12 +24,13 @@ export class EmailService {
 
   async sendEmail(options: SendMailOptions): Promise<boolean> {
     try {
-      const { to, subject, htmlBody } = options;
+      const { to, subject, htmlBody, attachements } = options;
 
       const sendInformation = await this.transporter.sendMail({
         to: to,
         subject: subject,
         html: htmlBody,
+        attachments: attachements,
       });
 
       console.log(sendInformation);
@@ -32,5 +38,27 @@ export class EmailService {
     } catch (error) {
       return false;
     }
+  }
+
+  async sendEmailWithFileSystemLogs(to: string | string[]) {
+    const subject = "Logs del servidor";
+    const htmlBody = `
+    <h3>Logs del sistema - NOC</h3>
+    <p> Probando 123, Probando 123, Probando 123, Probando 123. </p>
+    <p>Ver logs adjuntos</p>
+    `;
+
+    const attachements: Attachement[] = [
+      { filename: "logs-all.log", path: "./logs/logs-all.log" },
+      { filename: "logs-high.log", path: "./logs/logs-high.log" },
+      { filename: "logs-medium.log", path: "./logs/logs-medium.log" },
+    ];
+
+    return this.sendEmail({
+      to,
+      subject,
+      attachements,
+      htmlBody,
+    });
   }
 }

12405c89151ba8518368b7250b6f773465f0b6e8
Lautaro Decima <decimalautaro@gmail.com>
2024-03-30 16:48:20 -0300

    adding mongoose to app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,16 @@
 import { enviroment } from "./config/enviroment";
+import { MongoDatabase } from "./data/mongodb";
 import { Server } from "./presentation/server";
 
-async () => {
+(async () => {
   main();
-};
+})();
 
-function main() {
-  Server.start();
+async function main() {
+  await MongoDatabase.connect({
+    mongoUrl: enviroment.MONGO_URL,
+    dbName: enviroment.MONGO_DB_NAME,
+  });
+
+  // Server.start();
 }
